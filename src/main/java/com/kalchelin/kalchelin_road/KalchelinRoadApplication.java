c6353a08ae266e7cb3d146d3764d43d4afa9baa9
Seungmin Baek <bsm77125@naver.com>
2026-06-30 18:25:49 +0900

    init: kalchelin_road 프로젝트 생성

---
+++ src/main/java/com/kalchelin/kalchelin_road/KalchelinRoadApplication.java
@@ -0,0 +1,13 @@
+package com.kalchelin.kalchelin_road;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalchelinRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalchelinRoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalchelin/kalchelin_road/KalchelinRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalchelin.kalchelin_road;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalchelinRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
