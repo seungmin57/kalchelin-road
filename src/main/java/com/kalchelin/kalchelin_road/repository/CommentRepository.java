c0b07499f5918fe4103ed7ca584d9f243aeb9d41
Seungmin Baek <bsm77125@naver.com>
2026-07-30 19:51:07 +0900

    refactor: 글·댓글 목록 조회 N+1 해결

--- src/main/java/com/kalchelin/kalchelin_road/repository/CommentRepository.java
+++ src/main/java/com/kalchelin/kalchelin_road/repository/CommentRepository.java
@@ -1,11 +1,13 @@
 package com.kalchelin.kalchelin_road.repository;
 
 import com.kalchelin.kalchelin_road.entity.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 특정 글의 댓글만 조회 - 메서드 이름으로 쿼리
+    @EntityGraph(attributePaths = "author")
     List<Comment> findByPostId(Long postId);
 }
--- src/main/java/com/kalchelin/kalchelin_road/repository/PostRepository.java
+++ src/main/java/com/kalchelin/kalchelin_road/repository/PostRepository.java
@@ -1,7 +1,14 @@
 package com.kalchelin.kalchelin_road.repository;
 
 import com.kalchelin.kalchelin_road.entity.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Override
+    @EntityGraph(attributePaths = "author")
+    // 몸통은 여전히 Spring Data가 만들어주고, 우리는 "author도 같이 채워와라"만 얹는다.
+    List<Post> findAll();
 }
